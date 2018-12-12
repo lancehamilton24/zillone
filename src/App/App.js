050f76c44e8c4c8a4871fb379f79467e2f6bd08f
Lance Hamilton <lance.hamilton24@gmail.com>
2018-12-11 19:34:55 -0600

    added connection.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,36 +1,19 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
-import logo from './logo.svg';
-import './App.css';
+import connection from '../helpers/data/connection';
+
+import Auth from '../components/Auth/Auth';
+
+import './App.scss';
 
 class App extends Component {
+  componentDidMount() {
+    connection();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <button className='btn btn-danger'>HELP ME</button>
-          <Button
-            tag="a"
-            color="success"
-            size="large"
-            href="http://reactstrap.github.io"
-            target="_blank"
-            >
-              View Reactstrap Docs
-            </Button>      
-      </header>
+        <Auth></Auth>
       </div>
     );
   }
