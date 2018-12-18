e7bef8708d9ff030f88c68fd21960cd678d05ebe
Lance Hamilton <lance.hamilton24@gmail.com>
2018-12-17 19:01:48 -0600

    tbc

--- src/components/MyNavbar/MyNavbar.js
+++ src/components/MyNavbar/MyNavbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Collapse,
   Navbar,
@@ -11,22 +12,28 @@ import {
 import './MyNavbar.scss';
 
 class MyNavbar extends React.Component {
+  static propTypes = {
+    isAuthed: PropTypes.bool,
+    logoutClickEvent: PropTypes.func,
+  }
+
   state = {
     isOpen: false,
   };
 
   render() {
+    // const monkeyButt = this.props.isAuthed;
+    // const newThing = this.props.newThing;
     const { isAuthed, logoutClickEvent } = this.props;
     return (
       <div className="my-navbar">
-        <Navbar color="dark" dark expand="md">
+      <Navbar color="dark" dark expand="md">
           <NavbarBrand href="/">Zillone</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
                 { isAuthed ? <NavLink onClick={logoutClickEvent}>Logout</NavLink> : ''}
-                {/* <NavLink href="/components/">Logout</NavLink> */}
               </NavItem>
             </Nav>
           </Collapse>
